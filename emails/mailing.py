93d97116ff396c7a2b521023deb1abe1352ab1f2
Victor Trejo <vic3jo@gmail.com>
2021-01-21 08:14:21 -0400

    Document

--- emails/mailing.py
+++ emails/mailing.py
@@ -1,5 +1,6 @@
 import smtplib
 
+
 PROVIDERS_ADDRESSES = {
   'gmail': 'smtp.gmail.com',
   'hotmail': 'smtp.live.com',
@@ -15,11 +16,25 @@ class EmailSender:
     provider = 'gmail',
     port = 587,
   ):
+    """Constructor
+
+    Args:
+        user (string): User's email address
+        password (string): User's password
+        provider (str, optional): Email provider. Defaults to 'gmail'.
+        port (int, optional): Connection port. Defaults to 587.
+    """    
     self.__user = user
     self.__password = password
     self.__connection = smtplib.SMTP(PROVIDERS_ADDRESSES[provider], port = port)
 
   def send_mail(self, email, message):
+    """Sends an email a destination address
+
+    Args:
+        email (string): Destination email address
+        message (string): Message content
+    """    
     conn = self.__connection
     conn.starttls()
     conn.login(user = self.__user, password = self.__password)
